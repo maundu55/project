803b41c927405a64b577fe6a1b6ae02324d6ef81
maundu55 <marymaundu.mm@gmail.com>
2024-11-19 12:54:28 +0300

    create count using useReducer hook

--- src/components/ReducerTutorial.jsx
+++ src/components/ReducerTutorial.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const reducer=(state, action)=>{
+    switch(action.type){
+        case 'INCREMENT':
+            return {count: state.count+1, showText: state.showText};
+        case 'toggleShowText':
+            return {count: state.count, showText: !state.showText};
+        default:
+            return state;
+    }
+};
 
 const ReducerTutorial = () => {
+    const[state, dispatch] = useReducer(reducer, {count: 0, showText: true});
   return (
     <div>
         <h1>use Reducer</h1>
+        <h3>{state.count}</h3>
+        <button onClick={()=>{
+            dispatch({ type: 'INCREMENT'});
+            dispatch({ type: 'toggleShowText'});
+        }}>
+            Click Here
+        </button>
+        {state.showText && <p>This is a Text</p>}
     </div>
   )
 }
